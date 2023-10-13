a646c35241beeb8ac87c32e7157043a337295997
​ <mail.qt.kaneko@gmail.com>
2023-10-13 09:51:57 +0300

    ​

--- build.js
+++ build.js
@@ -1,20 +1,32 @@
 (async function() { "use strict";
 
+var util = require(`util`);
 var process = require("process");
-var child_process = require(`child_process`);
+var cp = require(`child_process`);
 var fs = require(`fs/promises`);
 
-let include = [`package.json`, `LICENSE`, `README.md`, `ICON.png`];
+let release = process.argv.includes(`release`);
+
+let include = [`package.json`, `LICENSE`, `README.md`, `assets/icon.png`];
 let destination = `dist`;
+
+if (release)
+{
+  await Promise.all([
+    util.promisify(cp.exec)(`tsc --build --clean tsconfig.json`),
+    util.promisify(cp.exec)(`tsc --build --clean tsconfig.release.json`)
+  ]);
+  await fs.rm(destination, {recursive: true, force: true});
+}
 
 await Promise.all([
   ...include.map(includeItem =>
     fs.cp(includeItem, destination + `/` + includeItem, {recursive: true})
   ),
-  new Promise(resolve =>
-    child_process.spawn(`tsc`, {stdio: `inherit`})
-                 .on(`exit`, resolve)
-  )
+  util.promisify(cp.spawn)(`tsc`, [
+    `--build`,
+    (release ? `tsconfig.release.json` : `tsconfig.json`)
+  ], {stdio: `inherit`})
 ]);
 
 })();
